f7a552dc2489d54154ee98253fefa1e6ddfa47a4
Alvaro Ruiz <alvaro.ruiz@bamboofinance.co.uk>
2016-01-16 07:25:12 +0000

    Initial changes and layout

--- config.rb
+++ config.rb
@@ -56,16 +56,17 @@ end
 # end
 
 # Add bower's directory to sprockets asset path
-after_configuration do
-  @bower_config = JSON.parse(IO.read("#{root}/.bowerrc"))
-  sprockets.append_path File.join "#{root}", @bower_config["directory"]
-end
+# after_configuration do
+#   @bower_config = JSON.parse(IO.read("#{root}/.bowerrc"))
+#   sprockets.append_path File.join "#{root}", @bower_config["directory"]
+# end
 
 # Use the correct vendor prefixes for foundation
 activate :autoprefixer do |config|
   config.browsers = ['last 2 versions', 'ie >= 9', 'and_chr >= 2.3']
 end
 
+# Asset Settings
 set :css_dir, 'stylesheets'
 
 set :js_dir, 'javascripts'
@@ -81,16 +82,16 @@ ignore "javascripts/script.js"
 # Build-specific configuration
 configure :build do
   # For example, change the Compass output style for deployment
-  # activate :minify_css
+  activate :minify_css
 
   # Minify Javascript on build
-  # activate :minify_javascript
+  activate :minify_javascript
 
   # Enable cache buster
   # activate :asset_hash
 
   # Use relative URLs
-  # activate :relative_assets
+  activate :relative_assets
 
   # Or use a different image path
   # set :http_prefix, "/Content/images/"
